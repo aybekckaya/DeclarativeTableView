c0958901d8b354e10d496647ee9597479e6a6be9
aybek can kaya <aybek.kaya@rapsodo.com>
2021-07-25 00:54:29 +0300

    Package Update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DeclarativeTableView",
+    platforms: [.macOS(.v10_10), .iOS(.v13), .watchOS(.v2), .tvOS(.v9) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
